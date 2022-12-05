b59098c38576857ee442d830a932eed6620dbfa5
Jake li <lyfolyf@163.com>
2022-12-05 10:29:51 +0800

    添加项目文件。

---
+++ ImageResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lead.Tool.Resources
+{
+    public class ImageManager
+    {
+        public static Image GetImage(string Name)
+        {
+            ResourceManager manager = new ResourceManager(typeof(Resource1));
+            return (Image)manager.GetObject(Name);
+        }
+    }
+}
